9cd7b8b63704f633d5d18e43ca98c11b069a85a2
shiryauwu <shamazik24005@gmail.com>
2023-05-14 15:14:39 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace TestTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(204, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Узнать погоду!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(393, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(393, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(393, 183);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 23);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(393, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Температура";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(393, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Описание";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(393, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Скорость ветра";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(29, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(185, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Введите название города";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(29, 67);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(185, 23);
+            this.textBox7.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 299);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Узнаватель погоды";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        
+        private Label label7;
+        private TextBox textBox7;
+    }
+}
---
+++ TestTask/Form1.cs
@@ -0,0 +1,83 @@
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Security.Policy;
+
+namespace TestTask
+{
+    public partial class Form1 : Form
+    {
+        string appId = "81d82e16b8085a7d65e60d6401116ec5";  //ключ API
+        string units = "metric";  //Данные для вывода температуры по цельсию
+        string lang = "ru";  // Язык вывода: русский
+        
+        HttpClient httpClient = new HttpClient();  //Создание HTTP клиента для дальнейшей работы с вебом
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string GetTemperature(string cityName)  //Метод для вывода температуры
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&APPID={appId}&units={units}&lang={lang}";  //ссылка для доступа к API
+            try
+            {
+                var response = httpClient.GetStringAsync(url).Result;  // Присваивание переменной данных с API
+                var responseMain = JObject.Parse(response).GetValue("main").ToString();  //Десериализация JSON
+                var responseTemp = JObject.Parse(responseMain).GetValue("temp").ToString();
+                return $"{responseTemp} °C";  //Вывод результата
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+        public string GetWeatherDescription(string cityName)
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&APPID={appId}&units={units}&lang={lang}";  //ссылка для доступа к API
+            try
+            {
+                var response = httpClient.GetStringAsync(url).Result;  // Присваивание переменной данных с API
+                var responseWeather = JObject.Parse(response).GetValue("weather");  //Десериализация JSON
+                var responseWeatherArray = responseWeather[0].ToString();
+                var responseDescription = JObject.Parse(responseWeatherArray).GetValue("description").ToString();
+                return responseDescription;  //Вывод результата
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+        public string GetWindSpeed(string cityName)
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&APPID={appId}&units={units}&lang={lang}";  //ссылка для доступа к API
+            try
+            {
+                var response = httpClient.GetStringAsync(url).Result;  // Присваивание переменной данных с API
+                var responseWind = JObject.Parse(response).GetValue("wind").ToString();  //Десериализация JSON
+                var responseSpeed = JObject.Parse(responseWind).GetValue("speed").ToString();
+                return $"{responseSpeed} км/ч";  //Вывод результата
+            }
+            catch (Exception ex)
+            {
+                
+                return "";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)  // Обработка события нажатия на кнопку
+        {
+            string cityName;  
+
+            cityName = textBox7.Text;  // Ввод пользователем названия города
+
+            textBox1.Text = GetTemperature(cityName);  // Присваивание полю результата метода
+            textBox2.Text = GetWeatherDescription(cityName);  // Присваивание полю результата метода
+            textBox3.Text = GetWindSpeed(cityName);  // Присваивание полю результата метода
+            if (textBox1.Text == "")  //  Обработка ошибки 
+            {
+                MessageBox.Show("Город не найден!");
+            }
+        }
+
+       
+    }
+}
